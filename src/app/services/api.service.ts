5d8b17f493554d4b53ce69a850d15fbe979d9f37
Yunus SANDIKCI <yunussandikci@outlook.com>
2019-04-15 19:48:38 +0300

    express added. api url added.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 
-const endpoint = 'http://localhost:8080/api/v1/';
+const endpoint = 'https://githubimporter-api.herokuapp.com/api/v1/';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
